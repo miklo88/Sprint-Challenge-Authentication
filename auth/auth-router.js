4315097898be328c5e1f4b10b9f7f4fb5db4e8e2
Carlos <miklo.concepcion@gmail.com>
2020-01-24 20:19:12 -0600

    login functionality defined. working on authenticating middleware then to jokes router.

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,44 @@
-const router = require('express').Router();
+// MIDDLEWARE
+const router = require("express").Router();
+// BRINGING IN JWT
+const jwt = require("jsonwebtoken");
 
-router.post('/register', (req, res) => {
+router.post("/register", (req, res) => {
   // implement registration
 });
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
   // implement login
+  let { username, password } = res.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+
+        res.status(200).json({
+          message: `Welcome ${user.username}!, have a token`,
+          token
+        });
+      } else {
+        res.status(401).json({ message: "Invalid Credentials" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 });
 
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username
+  };
+
+  const options = {
+    expiresIn: "1d"
+  };
+  return jwt.sign(payload, secrets.jwtSecret, options);
+}
 module.exports = router;
